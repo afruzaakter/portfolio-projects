60e6db4cdb5cdd8cc095e730e791756dfbb39957
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-06-09 08:26:16 +0600

    install package

---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   daisyui: {
+//     themes: [
+//       {
+//         mytheme: {
+        
+// "primary": "#3ABFF8",
+        
+// "secondary": "#D926A9",
+        
+// "accent": "#1FB2A6",
+        
+// "neutral": "#6419E6",
+        
+// "base-100": "#2A303C",
+        
+// "info": "#3ABFF8",
+        
+// "success": "#36D399",
+        
+// "warning": "#FBBD23",
+        
+// "error": "#F87272",
+//         },
+//       },
+//       "light",
+//     ],
+//   },
+  
+//   plugins: [require("daisyui")],
+// }
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#3ABFF8",
+
+          "secondary": "#D926A9",
+
+          "accent": "#1FB2A6",
+
+          "neutral": "#6419E6",
+
+          "base-100": "#2A303C",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+      "light",
+      
+    ], 
+  },
+  plugins: [require("daisyui")],
+}
